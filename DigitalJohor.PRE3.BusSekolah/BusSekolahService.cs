224cb31113b4bd2279deb8eab37f427793416958
amiryahaya <amirrudin.yahaya@gmail.com>
2021-07-15 23:28:28 +0800

    Bus Sekolah

---
+++ DigitalJohor.PRE3.BusSekolah/BusSekolahService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DigitalJohor.PRE3.DataImport;
+using DigitalJohor.PRE3.EFCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DigitalJohor.PRE3.BusSekolah
+{
+    public class BusSekolahService : IImportService
+    {
+        private readonly DigitalJohorPRE3DbContext _dbContext;
+        private readonly ILogger<BusSekolahService> _logger;
+        private readonly BusSekolahSettings _settings;
+
+        public BusSekolahService(
+            DigitalJohorPRE3DbContext dbContext,
+            ILogger<BusSekolahService> logger,
+            IOptions<BusSekolahSettings> settingsOptions)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+            _settings = settingsOptions.Value;
+        }
+    }
+}
---
+++ DigitalJohor.PRE3.BusSekolah/BusSekolahSettings.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DigitalJohor.PRE3.BusSekolah
+{
+    public class BusSekolahSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
